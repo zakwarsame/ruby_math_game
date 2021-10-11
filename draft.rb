efbd5bf21660d501f93c9ab1dff50f819f6e6493
zak <zakwarsamee@gmail.com>
2021-10-11 18:45:53 -0400

    create question inside game for now to test

--- draft.rb
+++ draft.rb
@@ -5,6 +5,9 @@ class Player
         @lives = 3
     end
 
+    def is_dead?
+        lives > 0 ? true : false
+    end
 
 end
 
@@ -12,17 +15,20 @@ class Game
 
     include Question
 
-    
-
+    def initialize(p1, p2)
+        @p1 = p1
+        @p2 = p2
+    end
 
 end
 
 module Question
-    
+    random_number = rand(0...20)
+    "What does ${}"
 end
 
 # when creating an instance, a player gets a name, a default 3 lives, a dead method, 
 
 # a game takes in 2 players, will include a question, method to check if they answered correctly or not, method that will switch turns between players, 
 
-# question generates a random question each time it's called
+# question generates a random question each time it's called, has a method 
--- game.rb
+++ game.rb
@@ -1,10 +1,31 @@
 
 module Question
-    puts "here is a question"
+
+    attr_accessor :first, :second
+
+    def initialize
+        @first, @second =  2.times.map { rand(20) }
+        @sum = @first + @second
+    end
+    
+    def generate_question(name)
+        puts "#{name} What is #{self.first} + #{self.second}?"
+    end
 end
 
 class Game
+    
+    def initialize(p1, p2)
+        @p1 = p1
+        @p2 = p2
+    end
+
+    
     include Question
+
+
 end
 
 mygame = Game.new
+mygame.generate_question
+p mygame.sum
